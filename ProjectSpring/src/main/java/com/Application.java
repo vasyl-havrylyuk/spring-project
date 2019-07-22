629c7a5b319d6fca0f085caffe30eb1ec789f58f
Vasyl Havrylyuk <vasyl.havrylyuk@sisnet360.com>
2019-07-22 09:50:49 +0200

    Plantilla spring

---
+++ ProjectSpring/src/main/java/com/Application.java
@@ -0,0 +1,21 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@EnableAutoConfiguration
+public class Application {
+
+	@RequestMapping("/")
+	String raiz() {
+		return "Hello World!!";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
